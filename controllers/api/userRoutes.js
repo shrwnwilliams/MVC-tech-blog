6dfed4729e2d640ab9bed805fb2919d9c72ea116
DINNERman <shrwnwilliams@gmail.com>
2021-04-26 18:15:26 -0400

    got the server running and successfully posted data to the api/users, began work on password validation

---
+++ controllers/api/userRoutes.js
@@ -0,0 +1,36 @@
+const router = require("express").Router();
+const { User } = require("../../models");
+
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+           
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
+
+router.post("/login", async (req, res) => {
+    try {
+        const userData = await User.findOne({ where: { email: req.body.email } });
+
+        if (!userData) {
+            res.status(400).json({ message: "Incorrect email or password, please try again" });
+            return;
+        }
+
+        const validPassword = await userData.checkPassword(req.body.password);
+
+
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const path = require('path');
 const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
-// const routes = require('./controllers');
+const routes = require("./controllers");
 // const helpers = require('./utils/helpers');
 
 const sequelize = require('./config/connection');
@@ -24,7 +24,7 @@ const sess = {
   })
 };
 
-// app.use(session(sess));
+app.use(session(sess));
 
 // Inform Express.js on which template engine to use
 // app.engine('handlebars', hbs.engine);
@@ -34,7 +34,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use(routes);
+app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
