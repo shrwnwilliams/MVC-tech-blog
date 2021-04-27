2cf06f209c90fe58f3e3ce31ba652c8ce2ecc881
DINNERman <shrwnwilliams@gmail.com>
2021-04-26 20:37:48 -0400

    got homepage to show up with handlebars

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,7 +1,10 @@
 const router = require("express").Router();
 const { User } = require("../../models");
 
-router.post("/register", async (req, res) => {
+
+
+
+router.post("/", async (req, res) => {
   try {
     const userData = await User.create(req.body);
 
--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -2,14 +2,9 @@ const router = require("express").Router();
 const { Post, User } = require("../models");
 const withAuth = require("../utils/auth");
 
-// router.get("/", async (req, res) => {
-//     try {
-        
-
-//     } catch (err){
-//         res.status(500).json(err);
-//     }
-// })
+router.get("/", async (req, res) => {
+   res.render("homepage");
+})
 
 
 
